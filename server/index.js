8d28ff8e0174b5c05e6f3cfdf54699207cdfe712
nicos <doger.ns@gmail.com>
2023-11-16 12:07:38 -0400

    Add socket connection

--- server/index.js
+++ server/index.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const path = require("path");
-
 const port = process.env.PORT || 3001;
-
 const app = express();
+
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "..", "public/index.html")));
 
-const server = app.listen(port, () => console.log(`App listening on port ${port}!`));
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
+server.listen(port, () => console.log(`App listening on port ${port}!`));
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
