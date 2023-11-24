4a39b41cf26a27b7539c35b0fa3de8b30b3403ae
nicos <doger.ns@gmail.com>
2023-11-24 11:40:07 -0400

    Add static files dir

--- server/index.js
+++ server/index.js
@@ -8,7 +8,10 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "..", "public/index.html")));
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 io.on('connection', (socket) => {
     console.log('a user connected');
